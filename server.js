2c76f458722c218254035ec5e3383e0d1af52dc2
Connor Cote <cotec4@gmail.com>
2020-08-20 19:35:07 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const path = require("path");
+
+// Sets up the Express App
+// =============================================================
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Data
+
+
+// Routes
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+  app.get("/tables", (req, res) => {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+  
+  app.get("/reserve", (req, res) => {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
