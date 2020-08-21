2dbae006e6700751be3198981871d3b045351efd
hsung8 <hsung@atlantaregional.org>
2020-08-20 21:26:35 -0400

    wip

--- server.js
+++ server.js
@@ -16,16 +16,23 @@ app.use(express.json());
 
 
 // Routes
+// api routes
+require("./routes/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+  console.log("App listening on http://localhost:"+ PORT)
+  console.log(__dirname)
+})
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "/public/home.html"));
   });
   
   app.get("/tables", (req, res) => {
-    res.sendFile(path.join(__dirname, "tables.html"));
+    res.sendFile(path.join(__dirname, "/public/tables.html"));
   });
   
   app.get("/reserve", (req, res) => {
-    res.sendFile(path.join(__dirname, "reserve.html"));
+    res.sendFile(path.join(__dirname, "/public/reserve.html"));
   });
